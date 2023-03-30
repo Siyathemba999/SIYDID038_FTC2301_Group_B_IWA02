b686d13915ba51939c33a463eb675471fb7912ba
Siyathemba.999 <siyathembacj@gmail.com>
2023-03-30 11:38:18 +0200

    challenge 2 done

---
+++ IWA_3.2Challenges/alex.js
@@ -0,0 +1,17 @@
+// alex.js
+
+const firstname = "Alex"
+const surname = "Hunter"
+export const role = "Head of Marketing"
+
+const display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
+
+/*// alex.js
+
+private firstname = "Alex"
+private surname = "Naidoo"
+public role = "Head of Marketing"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display */
---
+++ IWA_3.2Challenges/nwabisa.js
@@ -0,0 +1,17 @@
+// nwabisa.js
+
+const firstname = "Nwabisa"
+const surname = "Gabe"
+export const role = "CEO"
+
+const display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#nwabisa').innerText = display
+
+/*// nwabisa.js
+
+private firstname = "Nwabisa"
+private surname = "Gabe"
+public role = "CEO"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#nwabisa').innerText = display */
