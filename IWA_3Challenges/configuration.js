69f47a81cd4a91c45a2b21850f665c0e232161ba
Siyathemba.999 <siyathembacj@gmail.com>
2023-03-30 09:59:23 +0200

    all done

---
+++ IWA_3Challenges/configuration.js
@@ -0,0 +1,9 @@
+// configuration.js
+
+export const company = 'ACME Inc.'
+export const year = 2022
+
+/*// configuration.js
+
+const company = 'ACME Inc.'
+const year = 2022 */
---
+++ IWA_3Challenges/scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+import {company} from './configuration.js'
+import {year} from './configuration.js'
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText=message
+
+/*// scripts.js
+
+import company form 'configuration'
+import year form 'configuration'
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message */
