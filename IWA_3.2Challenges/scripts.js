ed06f006791123308f00026152da644096ccc168
Siyathemba.999 <siyathembacj@gmail.com>
2023-04-04 07:39:09 +0200

    iwa5 done

--- IWA_3.2Challenges/scripts.js
+++
@@ -1,7 +0,0 @@
-//script.js
-
-import { role as role1 } from './nwabisa.js'
-import { role as role2 } from './johannes.js'
-import { role as role3 } from './alex.js'
-
-console.log('Roles:', role1,',', role2,',', role3)
--- IWA_3Challenges/configuration.js
+++
@@ -1,9 +0,0 @@
-// configuration.js
-
-export const company = 'ACME Inc.'
-export const year = 2022
-
-/*// configuration.js
-
-const company = 'ACME Inc.'
-const year = 2022 */
